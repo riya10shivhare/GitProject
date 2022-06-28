c64867f7f8741c47aaddc20b957c9a6eb24a5a89
riya10shivhare <sonal.shivhare15@outlook.com>
2022-06-27 22:13:06 -0700

    info and url function

---
+++ src/com/facebook/modul/DisplayInfo.java
@@ -0,0 +1,15 @@
+package com.facebook.modul;
+
+public class DisplayInfo {
+		
+	public static String infoName() {
+		return "Facebook";
+	}
+	public  static String url() {
+		return "https:\\www.facebook.com";
+	}
+	public static void main(String[] args) {
+		System.out.println(infoName());
+		System.out.println(url());
+	}
+}
